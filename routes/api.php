a3622bbf7530bca7954d8fe167d1f0773e0f3356
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-02 09:06:30 +0600

    dummy user created

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ArticleController;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,3 +26,17 @@ Route::get( '/articles{id}', [ArticleController::class, 'getArticle'] );
 Route::post( '/articles', [ArticleController::class, 'createArticle'] );
 Route::put( '/articles{id}', [ArticleController::class, 'updateArticle'] );
 Route::delete( '/articles{id}', [ArticleController::class, 'deleteArticle'] );
+
+Route::get( '/create', function () {
+    User::forceCreate( [
+        'name'     => 'John Doe',
+        'email'    => 'john@doe.com',
+        'password' => Hash::make( 'abcd1234' ),
+    ] );
+
+    User::forceCreate( [
+        'name'     => 'Jane Doe',
+        'email'    => 'jane@doe.com',
+        'password' => Hash::make( 'abcd1234' ),
+    ] );
+} );
