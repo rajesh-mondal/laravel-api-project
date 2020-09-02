afd0f76ce59f45d7d208f710aa2e0b6409245ef4
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-02 08:22:25 +0600

    api route created

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get( '/articles', [ArticleController::class, 'getAllArticles'] );
+Route::get( '/articles{id}', [ArticleController::class, 'getArticle'] );
+Route::post( '/articles', [ArticleController::class, 'createArticle'] );
+Route::put( '/articles{id}', [ArticleController::class, 'updateArticle'] );
+Route::delete( '/articles{id}', [ArticleController::class, 'deleteArticle'] );
